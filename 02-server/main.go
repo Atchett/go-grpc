7db9fc8a28db3aaeca39ec72bbc403bc3dde4ef1
Atchett <Atchett@users.noreply.github.com>
2020-03-07 18:05:25 +0000

    added server code

--- 02-server/main.go
+++ 02-server/main.go
@@ -2,14 +2,38 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"net"
 
-	"github.com/Atchett/grpc/02-server/echo"
+	"github.com/Atchett/go-grpc/02-server/echo"
+	"google.golang.org/grpc"
 )
 
 // EchoServer - empty struct
 type EchoServer struct{}
 
 // Echo - function
-func (e *EchoServer) Echo(context.Context, *echo.EchoRequest) (*echo.EchoResponse, error)
+func (e *EchoServer) Echo(ctx context.Context, req *echo.EchoRequest) (*echo.EchoResponse, error) {
+	return &echo.EchoResponse{
+		Response: "My Echo: " + req.Message,
+	}, nil
+}
 
-func main() {}
+func main() {
+	// create a network listner
+	lst, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		panic(err)
+	}
+	s := grpc.NewServer()
+
+	srv := &EchoServer{}
+
+	echo.RegisterEchoServerServer(s, srv)
+	fmt.Println("Now serving at port 8000")
+
+	err = s.Serve(lst)
+	if err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module Atchett/go-grpc
+module github.com/Atchett/go-grpc
 
 go 1.13
 
-require github.com/golang/protobuf v1.3.4
+require (
+	github.com/golang/protobuf v1.3.4
+	google.golang.org/grpc v1.27.1
+)
